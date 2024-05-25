8973017bde72d9eb4b1db4569154b46207435f34
David wanjohi <davidwagura83@gmail.com>
2024-05-25 11:45:07 +0300

    Import elements in js

---
+++ index.js
@@ -0,0 +1,9 @@
+const accessKey = "aEfn8IKe4XkMecE4Sir8r2Y20cv6_mlvfODHH2RCacs"
+
+const formEl = document.querySelector("form")
+
+const inputEl = document.getElementById("search-input")
+
+const searchResults = document.querySelector(".search results")
+
+const showMore = document.getElementById("show-more-button")
